fdef294afa753d0797ae40e0c79740e7f71464dc
Joost van der Griendt <joostvdg@gmail.com>
2023-03-25 14:50:48 +0100

    add exit code management with exitcode crate

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap_verbosity_flag::{Verbosity};
 use anyhow::{Context, Result};
 use log::{info};
 use env_logger::{Builder, Target};
+use std::process;
 
 #[macro_use]
 extern crate log;
@@ -19,7 +20,7 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main()  -> Result<()> {
+fn main()  {
     let args = Cli::parse();
 
     let mut builder = Builder::from_default_env();
@@ -29,12 +30,19 @@ fn main()  -> Result<()> {
     info!("Starting up");
     debug!("{:?}", args);
 
-    let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
+    let content_reading_result = std::fs::read_to_string(&args.path);
+    match content_reading_result {
+        Ok(content) => {
+            debug!("Reading file {}", &args.path.display());
+            git_next_tag::find_matches(&content, &args.pattern, &mut std::io::stdout());
+            process::exit(exitcode::OK);
+        },
+        Err(error) => {
+            error!("Could not read file `{}`: {}", &args.path.display(), error);
+            process::exit(exitcode::IOERR);
+        }
+    }
 
-    debug!("Reading file {}", &args.path.display());
-    git_next_tag::find_matches(&content, &args.pattern, &mut std::io::stdout());
-    Ok(())
 }
 
 
